b298c8de4254bb76c4ab5f1fa29ab4dea3b148f7
CrkJohn <Jhonhrge@gmail.com>
2019-04-08 17:37:41 -0500

    change struct project.

---
+++ src/main/java/com/example/controller/ViewController.java
@@ -0,0 +1,63 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping(value = { "/" , "index" })
+	  String index() {
+	    return "index";
+	 }
+	
+	@RequestMapping("/login")
+	String login() {
+	    return "login";
+	}
+	
+	@RequestMapping("/userHome")
+	String userHome() {
+	    return "userHome";
+	}
+	
+	
+	@RequestMapping("/configuracion")
+	String configuracion() {
+		return "configuracion";
+	}
+	
+	@RequestMapping("/pagos")
+	String pagos() {
+		return "pagos";
+	}
+	
+	@RequestMapping("/viajes")
+	String viajes() {
+		return "viajes";
+	}
+	
+	@RequestMapping("/cupones")
+	String cupones() {
+		return "cupones";
+	}
+	
+	@RequestMapping("/confConductor")
+	String confConductor() {
+		return "confConductor";
+	}
+	
+	@RequestMapping("/navbar")
+	String navbar() {
+		return "navbar";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
