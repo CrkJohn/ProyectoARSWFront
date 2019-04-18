7bb9059832e8596597446865eae734b9b160aa61
CrkJohn <Jhonhrge@gmail.com>
2019-04-18 00:15:06 -0500

    NO X,si no S
    Co-authored-by:  Velandia <jcamilovelandiab@gmail.com>
    Co-authored-by:  Karen <k26duran@gmail.com>

--- src/main/resources/static/js/stompConductor.js
+++ src/main/resources/static/js/stompConductor.js
@@ -18,7 +18,7 @@ var conductorViajes =(function () {
             '<h5 class="card-title">Viaje pedido por el usario : CrkJohn</h5>' +
             '<p class="card-text" id = "Inicio"> El usuario se debe recoger en : '+ message.lugarOrigen +'.</p>' +
             '<p class="card-text" id = "fin">        El usuerio tiene como destino :'+  message.lugarDestino +'</p>' +
-            '<p>      El usuario tiene una calificaion 3 extrellas.  </p>     <p class="card-text"><small class="text-muted"></small></p>' +
+            '<p>      El usuario tiene una calificaion 3 estrellas.  </p>     <p class="card-text"><small class="text-muted"></small></p>' +
             '</div>   <div class="card-footer text-center">' +
                 '<button type="button" class="btn  btn-lg btn-block" style = "background-color: #5ccfb1; color : white">Ver ruta</button>' +
                 '<button type="button" class="btn btn-danger btn-lg btn-block">Eliminar</button>'+
