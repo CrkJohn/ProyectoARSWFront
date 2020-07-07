2583893136dad9eda9467e53d0565a0a295d915d
CrkJohn <JhonhRge@gmail.com>
2020-07-06 21:15:36 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,11 +1,14 @@
-/**
- * @author <a href="mailto:ggsosa@transportsystems.co">Gerson Sosa</a>
- * @since 4.9.25
- */
-@Configuration
-public class SpaceRegistrationConfiguration {
 
-	static class WorkflowLeaseListener implements LeaseListener {
+// El formato coincide con kk:mm:ss EEE, MMM dd, yyyy
+Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w \\d*, \\d*");
 
-	}
+
+double distToLineSegment(point p, point a, point b, point &c) {
+  vec ap = toVec(a, p), ab = toVec(a, b);
+  double u = dot(ap, ab) / norm_sq(ab);
+  if (u < 0.0) { c = point(a.x, a.y);// closer to a
+    return dist(p, a); }// Euclidean distance between p and a
+  if (u > 1.0) { c = point(b.x, b.y);                   // closer to b
+    return dist(p, b); }         // Euclidean distance between p and b
+  return distToLine(p, a, b, c); 
 }
