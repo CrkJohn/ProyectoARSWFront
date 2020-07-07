53e0d814e410b12f83479a4d5911dea35bc46437
CrkJohn <JhonhRge@gmail.com>
2020-07-06 21:02:47 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -5,10 +5,7 @@
 @Configuration
 public class SpaceRegistrationConfiguration {
 
-	/**
-	 * @author <a href="mailto:ggsosa@transportsystems.co">Gerson Sosa</a>
-	 * @since 4.9.25
-	 */
+	
 	static class WorkflowLeaseListener implements LeaseListener {
 
 	}
