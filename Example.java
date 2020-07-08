80e6185712c3046a9effe07c4751e7036731f10e
CrkJohn <JhonhRge@gmail.com>
2020-07-08 15:06:06 -0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,8 +1,21 @@
-for(Node node : Graph[u]){
-  bfs(node)
-}
-
-AdjacentList adjList =  Graph[u];
-for(Node node : adjLsit){
-  bfs(node)
+public class DeviceController {  ... 
+    public void sendShutDown() {   
+      DeviceHandle handle = getHandle(DEV1);   
+      // Check the state of the device   
+      if (handle != DeviceHandle.INVALID) {  
+        // Save the device status to the record field  
+        retrieveDeviceRecord(handle);      
+        // If not suspended, shut down     
+        if (record.getStatus() != DEVICE_SUSPENDED) { 
+            pauseDevice(handle);        
+            clearDeviceWorkQueue(handle);   
+            closeDevice(handle);     
+        }else {       
+            logger.log("Device suspended.  Unable to shut down");     
+        }    
+      } else {    
+        logger.log("Invalid handle for: " + DEV1.toString());  
+      } 
+  }
+...
 }
